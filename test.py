26d7e8cc6914a4c8047014f3b722094b8c7002cc
Birger Möllering <bmoellering@gmail.com>
2020-04-28 16:14:04 +0200

    old test in another file

--- test.py
+++
@@ -1,9 +0,0 @@
-from asteroids import main, SpriteView
-
-
-if __name__ == "__main__":
-    win,world,ship = main()
-    sw = SpriteView("images/spaceship.png")
-    sw.register(ship)
-    for v in sw.draw():
-        print(v.delete)
